4434d48b13becfb05960850e96949c1ee6f3e044
tony <tony@nas.nas>
2012-08-27 20:01:14 +1000

    Finally adding D - was a submodule

---
+++ PARTD/includes/db_connect.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* MySQL connection check
+*
+* Checks the connection to the local install of MySQL
+*
+*/
+
+  define('DB_HOST', 'localhost');
+  define('DB_PORT', '3306');
+  define('DB_NAME', 'winestore');
+  define('DB_USER', 'wda');
+  define('DB_PW',   'password');
+
+?>
+
